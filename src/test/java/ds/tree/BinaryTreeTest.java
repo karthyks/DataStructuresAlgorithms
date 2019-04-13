2846e2e00a5cd3ea2aef643649d9b18a27cc6787
Karthik Periasami <karthikperiasami@Karthiks-MacBook-Pro.local>
2019-04-14 01:03:16 +0530

    implement binary tree and test cases

---
+++ src/test/java/ds/tree/BinaryTreeTest.java
@@ -0,0 +1,65 @@
+package ds.tree;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertTrue;
+
+public class BinaryTreeTest {
+
+    Integer[] smallArray;
+    Integer[] largerArray;
+
+    @Before
+    public void setUp() throws Exception {
+        File file = new File(getClass().getClassLoader().getResource("array/defaultArray.txt").getFile());
+        Scanner scanner = new Scanner(file);
+        smallArray = new Integer[scanner.nextInt()];
+        for (int i = 0; i < smallArray.length; i++) {
+            smallArray[i] = scanner.nextInt();
+        }
+
+        file = new File(getClass().getClassLoader().getResource("array/largeArray.txt").getFile());
+        scanner = new Scanner(file);
+        largerArray = new Integer[scanner.nextInt()];
+        for (int i = 0; i < largerArray.length; i++) {
+            largerArray[i] = scanner.nextInt();
+        }
+    }
+
+
+    @Test
+    public void checkBinaryTreePropertyWithSmallArray() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        for (int i = 0; i < smallArray.length; i++) {
+            tree.add(smallArray[i]);
+        }
+        tree.stream(node -> {
+            if (node.left != null) {
+                assertTrue("Left should be lesser than " + node.value, node.left.value.compareTo(node.value) <= 0);
+            }
+            if (node.right != null) {
+                assertTrue("Right should be greater than " + node.value, node.right.value.compareTo(node.value) >= 0);
+            }
+        });
+    }
+
+    @Test
+    public void checkBinaryTreePropertyWithLargeArray() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        for (int i = 0; i < largerArray.length; i++) {
+            tree.add(largerArray[i]);
+        }
+        tree.stream(node -> {
+            if (node.left != null) {
+                assertTrue("Left should be lesser than " + node.value, node.left.value.compareTo(node.value) <= 0);
+            }
+            if (node.right != null) {
+                assertTrue("Right should be greater than " + node.value, node.right.value.compareTo(node.value) >= 0);
+            }
+        });
+    }
+}
